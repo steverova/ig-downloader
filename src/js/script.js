d57a841f17d6607c2d01c7915af37e3a9df1b5c5
DESKTOP-HPJT45M\Steve <steverova0594@gmail.com>
2022-09-11 15:40:46 -0600

    valid

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+document.getElementById("uri").addEventListener("click", () => {
+  document.getElementById("myalert01").style.display="none";
+});
+
 function clearData() {
   document.getElementById("uri").value = "";
   document.getElementById("image1").src = "";
@@ -6,11 +10,24 @@ function clearData() {
 }
 
 function geturi() {
-  document.getElementById("image1").style.display = "block";
-  const uri = document.getElementById("uri").value + "media/?size=l";
-  document.getElementById("download").href = uri;
-  document.getElementById("image1").src = uri;
-  let newUri1 = new cDg("image1").view();
-  console.log("New_Image_Url 1: " + newUri1);
-  document.getElementById("card-img").style.display = "block";
+  const uri = document.getElementById("uri").value;
+
+  const ur = uri.split("/p/");
+  if (uri === "" ||
+   (ur[0] === "https://www.instagram.com" && ur[1].length <12)
+   || ur[0] != "https://www.instagram.com") {
+    document.getElementById(
+      "alert"
+    ).innerHTML = `<div id="myalert01" class="alert alert-warning" role="alert">
+  Ingrese una direccion valida
+</div>`;
+  } else {
+    const url = uri + "media/?size=l";
+    document.getElementById("image1").style.display = "block";
+    document.getElementById("download").href = url;
+    document.getElementById("image1").src = url;
+    let newUri1 = new cDg("image1").view();
+    console.log("New_Image_Url 1: " + newUri1);
+    document.getElementById("card-img").style.display = "block";
+  }
 }
