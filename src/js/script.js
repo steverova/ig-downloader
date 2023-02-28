fa884214b1893fd8ad463e861d6d5edc3edaf246
steverova <71293149+steverova@users.noreply.github.com>
2023-02-28 13:43:26 -0600

    download method

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 document.getElementById("uri").addEventListener("click", () => {
-  document.getElementById("myalert01").style.display="none";
+  document.getElementById("myalert01").style.display = "none";
 });
 
 function clearData() {
@@ -13,9 +13,11 @@ function geturi() {
   const uri = document.getElementById("uri").value;
 
   const ur = uri.split("/p/");
-  if (uri === "" ||
-   (ur[0] === "https://www.instagram.com" && ur[1].length <12)
-   || ur[0] != "https://www.instagram.com") {
+  if (
+    uri === "" ||
+    (ur[0] === "https://www.instagram.com" && ur[1].length < 12) ||
+    ur[0] != "https://www.instagram.com"
+  ) {
     document.getElementById(
       "alert"
     ).innerHTML = `<p id="myalert01">Ingrese una direccion valida</p>`;
@@ -28,4 +30,33 @@ function geturi() {
     console.log("New_Image_Url 1: " + newUri1);
     document.getElementById("card-img").style.display = "block";
   }
+}
+
+// function downloadImage() {
+//   var url = document.getElementById("image1").src;
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", url, true);
+//   xhr.responseType = "blob";
+//   xhr.onload = function () {
+//     var urlCreator = window.URL || window.webkitURL;
+//     var imageUrl = urlCreator.createObjectURL(this.response);
+//     var tag = document.createElement("a");
+//     tag.href = imageUrl;
+//     tag.download = "image.jpg";
+//     document.body.appendChild(tag);
+//     tag.click();
+//     document.body.removeChild(tag);
+//     window.URL.revokeObjectURL(imageUrl);
+//   };
+//   xhr.send();
+// }
+
+function downloadImage() {
+  const image = document.getElementById("image1");
+  const url = image.src;
+  const filename = "image.jpg";
+  const anchor = document.createElement("a");
+  anchor.download = filename;
+  anchor.href = url;
+  anchor.click();
 }
